c449bf3a18b78ec64da98802852cfc32a9a0d133
chriscauley <chris@lablackey.com>
2015-09-15 02:42:45 -0400

    get_absolute_url was broken

--- mptt_comments/urls.py
+++ mptt_comments/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url, patterns
 
 urlpatterns = patterns(
   'mptt_comments.views',
-  url('^(\d+)/$','detail'),
+  url('^(\d+)/$','detail',name="comment-detail-tree"),
   url('^list/$','list_comments'),
   url('^post/$','post'),
   url('^edit/(\d+)/$','edit'),
