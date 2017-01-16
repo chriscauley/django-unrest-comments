8c1ae4722bd9cd0e9fd0bd0172fd769a04eb92d4
chriscauley <chris@lablackey.com>
2017-01-16 16:43:37 -0500

    adding setup files

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = "django-mptt-comments",
-    version = '0.1.6',
-    url = 'http://github.com/justquick/django-mptt-comments/',
-    author = 'fivethreeo, justquick',
-    description = 'Django Mptt Comments is a simple way to display threaded comments instead of the django contrib comments.',
+    name = "django-unrest-comments",
+    version = '0.1.0',
+    url = 'https://github.com/chriscauley/django-unrest-comments/archive/v0.1.0.tar.gz',
+    author = 'chriscauley',
+    description = 'Threaded ajax comments using django on the back end and riot/unrest on the front.',
+    keywords = ['django','comments','threaded comments'],
     packages = find_packages(),
     include_package_data=True,
 )
