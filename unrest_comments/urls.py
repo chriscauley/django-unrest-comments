2956df67b9384be623939292adefb45c6ebe18b9
chriscauley <chris@lablackey.com>
2017-01-16 15:26:10 -0500

    naming urls

--- unrest_comments/urls.py
+++ unrest_comments/urls.py
@@ -4,9 +4,9 @@ from unrest_comments import views
 
 urlpatterns = [
   url('^(\d+)/$', views.detail,name="comment-detail-tree"),
-  url('^list/$', views.list_comments),
-  url('^post/$', views.post),
-  url('^edit/(\d+)/$', views.edit),
-  url('^delete/(\d+)/$', views.delete),
-  url('^flag/(\d+)/$', views.flag),
+  url('^list/$', views.list_comments, name="list-comments"),
+  url('^post/$', views.post, name="post-comment"),
+  url('^edit/(\d+)/$', views.edit, name="edit-comment"),
+  url('^delete/(\d+)/$', views.delete, name="delete-comment"),
+  url('^flag/(\d+)/$', views.flag, name="flag-comment"),
 ]
